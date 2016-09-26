6d8e890dd3d70dfdfca1baae3c34eac3c5d8c4f3
Brian Luc <bluc@Brians-MacBook-Pro.local>
2016-09-25 23:33:03 -0500

    tunes fixed

--- personal.js
+++ personal.js
@@ -1,22 +1,31 @@
 $(function() {
 
-    function startPlaying() {
-        $("#utcod")[0].play();
-    }
-
-
-    function stopPlaying() {
-        $("#utcod")[0].pause()
-    }
-
     
     $("#strokes").on('click', function() {
+        if ($("#utcod")[0].paused) {
+            $("#utcod")[0].play();
+        } else {
+            $("#utcod")[0].pause();
+        }
+    });
+
+    $("#cage").on('click', function() {
+        if ($("#cigday")[0].paused) {
+            $("#cigday")[0].play();
+        } else {
+            $("#cigday")[0].pause();
+        }
+    });
+
+    $("#head").on('click', function() {
         if ($("#waves")[0].paused) {
             $("#waves")[0].play();
         } else {
             $("#waves")[0].pause();
         }
     });
+
+
 
 
     
